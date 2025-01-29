5176d8ae087adcef670835e448ef77c5ff83fc2c
JuanRibeiro1 <juannopb@gmail.com>
2025-01-29 15:21:48 -0300

    Add files via upload

---
+++ digitarFuncao/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_While_pt2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeFuncao;
+            bool digitado = false;
+
+            Console.Write("Digite o nome da função (administrador, gerente ou usuario): ");
+
+            do
+            {
+                nomeFuncao = Console.ReadLine().Trim().ToLower();
+
+                if (nomeFuncao.Contains("administrador"))
+                {
+                    digitado = true;
+                }
+                else if (nomeFuncao.Contains("gerente"))
+                {
+                    digitado = true;
+                }
+                else if (nomeFuncao.Contains("usuario"))
+                {
+                    digitado = true;
+                }
+                else
+                {
+                    Console.Write($"Você digitou ({nomeFuncao}), função não reconhecida, digite outra (administrador, gerente ou usuario: ");
+                }
+            }
+            while (digitado == false);
+
+            Console.WriteLine($"Função {nomeFuncao} reconhecida.");
+        }
+    }
+}
